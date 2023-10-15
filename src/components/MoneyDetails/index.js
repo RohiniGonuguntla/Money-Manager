03549bf2f8ad8b10a26d2c772277b1d3549c3134
RohiniGonuguntla <rohinichowdary207@gmail.com>
2023-10-15 06:33:40 +0000

    Money Manager

---
+++ src/components/MoneyDetails/index.js
@@ -0,0 +1,52 @@
+// Write your code here
+import './index.css'
+
+const MoneyDetails = props => {
+  const {balanceAmount, incomeAmount, expensesAmount} = props
+
+  return (
+    <div className="main-container">
+      <div className="card border2">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+          alt="income"
+        />
+        <div>
+          <p className="heading">Your Income</p>
+          <p className="amount" data-testid="incomeAmount">
+            Rs. {incomeAmount}
+          </p>
+        </div>
+      </div>
+      <div className="card border3">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+          alt="expenses"
+        />
+        <div>
+          <p className="heading">Your Expenses</p>
+          <p className="amount" data-testid="expensesAmount">
+            Rs. {expensesAmount}
+          </p>
+        </div>
+      </div>
+      <div className="card border1">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          alt="balance"
+        />
+        <div>
+          <p className="heading">Your Balance</p>
+          <p className="amount" data-testid="balanceAmount">
+            Rs. {balanceAmount}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MoneyDetails
